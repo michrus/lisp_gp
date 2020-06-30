acf7b1e31965e0b7c545aadb84ee57511e6babdd
michrus <m.chruscinski1994@gmail.com>
2020-07-01 00:02:33 +0200

    Improved terminator generation

--- generate-random-program.rkt
+++ generate-random-program.rkt
@@ -12,14 +12,48 @@
 (define (get-random-element elements)
   (list-ref elements (random 0 (length elements))))
 
-(define (get-random-program functions terminators min-sub-elements max-sub-elements sub-probability)
+(define (get-input-symbol number)
+  (string->symbol (string-append "x" (number->string number)))
+  )
+
+(define (get-input-symbol-list inputs-count)
+  (let f ([result '()] [i inputs-count])
+    (if (zero? i)
+        result
+        (f (append result (list (get-input-symbol i))) (- i 1))
+     )
+    )
+  )
+
+(define (get-terminator input-symbols const-min const-max [const-probability 0.5])
+  (if (<= (random) const-probability)
+      (+ (random const-min const-max) (random))
+      (get-random-element input-symbols)
+   )
+  )
+
+(define (get-random-program
+         functions
+         input-symbols
+         const-min
+         const-max
+         min-sub-elements
+         max-sub-elements
+         sub-probability)
   (append (list (get-random-element functions))
    (let random-element ([program '()] [i (random min-sub-elements max-sub-elements)])
-     (if (zero? i)
+     (if (< i 0)
          program
          (if (<= (random) sub-probability)
-             (append program (list (get-random-program functions terminators min-sub-elements max-sub-elements sub-probability)))
-             (random-element (append program (list (get-random-element terminators))) (- i 1))
+             (append program (list (get-random-program
+                                    functions
+                                    input-symbols
+                                    const-min
+                                    const-max
+                                    min-sub-elements
+                                    max-sub-elements
+                                    sub-probability)))
+             (random-element (append program (list (get-terminator input-symbols const-min const-max))) (- i 1))
              )
          )
      )
