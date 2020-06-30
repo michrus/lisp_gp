4026018bd03c186dc495dce07a85fa07e4c64b14
michrus <m.chruscinski1994@gmail.com>
2020-07-01 00:16:44 +0200

    Added comments

--- generate-random-program.rkt
+++ generate-random-program.rkt
@@ -1,21 +1,21 @@
 #lang scheme
 
-(define (random-real min max [use-real #t])
-  (+ (random min max)
-     (if use-real
-         (random)
-         0
-     )
-  )
-)
-
+; gets random element from list provided as argument
+; args:
+;     elements    - list of elements to be randomly chosen from
 (define (get-random-element elements)
   (list-ref elements (random 0 (length elements))))
 
+; construct symbol for input variable
+; args:
+;     number    - input variable number
 (define (get-input-symbol number)
   (string->symbol (string-append "x" (number->string number)))
   )
 
+; gets list of input variable symbols
+; args:
+;     inputs-count    - number of input variables
 (define (get-input-symbol-list inputs-count)
   (let f ([result '()] [i inputs-count])
     (if (zero? i)
@@ -25,6 +25,12 @@
     )
   )
 
+; returns terminator element for program tree - either input variable or random constant
+; args:
+;     input-symbols        - list of symbols for input variables
+;     const-min            - minimal value for random constant
+;     const-max            - maximal value for random constant
+;     const-probability    - probability of picking random constant [default: 0.5]
 (define (get-terminator input-symbols const-min const-max [const-probability 0.5])
   (if (<= (random) const-probability)
       (+ (random const-min const-max) (random))
@@ -32,6 +38,15 @@
    )
   )
 
+; generate random program tree
+; args:
+;     functions        - list of function symbols to construct program from
+;     input-symbols    - list of symbols for input variables
+;     const-min        - minimal value for random constant (for get-terminator)
+;     const-max        - maximal value for random constant (for get-terminator)
+;     min-sub-elements - minimal number of elements in each sub-tree
+;     max-sub-elements - maximal number of elements in each sub-tree
+;     sub-probability  - probability of generating sub-program instead of pcking terminator
 (define (get-random-program
          functions
          input-symbols
@@ -59,6 +74,3 @@
      )
   )
 )
-
-(define functions '(+ - * /))
-(define terminators '(x1 x2 random-const))
